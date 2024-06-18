59ca077561ea901fa3fe2726a0688072a0ee7301
firmangel8 <firmangel8@gmail.com>
2024-06-18 11:21:16 +0800

    add new endpoints

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from extensions import jwt
 from api.books.endpoints import books_endpoints
 from api.auth.endpoints import auth_endpoints
 from api.data_protected.endpoints import protected_endpoints
+from api.fcm.endpoints import firebase_messaging
 from config import Config
 from static.static_file_server import static_file_server
+
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -26,6 +28,7 @@ app.register_blueprint(protected_endpoints,
                        url_prefix='/api/v1/protected')
 app.register_blueprint(books_endpoints, url_prefix='/api/v1/books')
 app.register_blueprint(static_file_server, url_prefix='/static/')
+app.register_blueprint(firebase_messaging, url_prefix="/api/v1/private/fcm")
 
 if __name__ == '__main__':
     app.run(debug=True)
